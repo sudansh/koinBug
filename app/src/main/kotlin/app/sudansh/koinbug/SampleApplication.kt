b29c35aba4aa11197d85c53328f293769acfca6c
Sudhanshu Singh <sudanshs@gmail.com>
2023-11-25 02:41:53 +0400

    koin

--- app/src/main/kotlin/app/sudansh/koinbug/SampleApplication.kt
+++ app/src/main/kotlin/app/sudansh/koinbug/SampleApplication.kt
@@ -1,17 +1,15 @@
 package app.sudansh.koinbug
 
 import android.app.Application
-import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
-import org.koin.ksp.generated.defaultModule as app
+import org.koin.ksp.generated.defaultModule
 
 class SampleApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
         startKoin {
-            androidContext(applicationContext)
-            modules(app)
+            defaultModule()
         }
     }
 }
